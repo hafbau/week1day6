231030360f972b9d3a5d15e1ffda8c56074bd6b4
Hafiz Suara <hafbau@yahoo.com>
2017-01-15 10:02:33 +0000

    streaming large text file

--- https_example.js
+++ https_example.js
@@ -1,3 +1,23 @@
 var https = require('https');
 
-console.log('idid it!')
+var options = {
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log(chunk.toString());
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  });
+}
+
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
